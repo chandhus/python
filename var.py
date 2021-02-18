9bc64c20ae9c6d8c38d2438ff76f28f4768c8978
chandhus <64320010+chandhus@users.noreply.github.com>
2021-02-18 17:37:10 +0800

    Create var.py

---
+++ var.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 18 17:12:46 2021
+
+@author: ksara
+"""
+
+my_ip = "143.168.0.102"
+network_addr = "143.168.0."
+start_addr = "143.168.0.1"
+end_addr = "143.168.0.255"
+curr_addr = start_addr
+
+num_of_threads = 1
+active_ip_list = []
+total_time = ""
+thread_ip_list = []
+thread_exec_time = []
